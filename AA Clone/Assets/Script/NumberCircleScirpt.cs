3a8063de0e115ef506bb92de7d672c08f16b4293
Mücahit Aktürk <m.akturk6134@gmail.com>
2021-03-11 12:07:18 +0300

    AAProject

---
+++ AA Clone/Assets/Script/NumberCircleScirpt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberCircleScirpt : MonoBehaviour
+{
+    [SerializeField] private GameObject smallCircle;
+    [SerializeField] private int howManyCircle;
+    private bool CircleControll = false;
+    [SerializeField] private int levelScene;
+    [SerializeField] private Text howManyCircleText;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(smallCircle, transform.position, transform.rotation);
+            howManyCircle--;
+            howManyCircleText.text = howManyCircle.ToString();
+        }
+        if (howManyCircle == 0)
+        {
+            CircleControll = true;
+            SceneManager.LoadScene(levelScene+1);
+        }
+
+    }
+}
---
+++ AA Clone/Assets/Script/SmallCircleScirpt.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SmallCircleScirpt : MonoBehaviour
+{
+
+    private Rigidbody2D Rigidbody2D;
+    [SerializeField] private int speed = 5;
+    private bool controll = false;
+    [SerializeField] private int gameLevel;
+
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (!controll)
+        {
+            Rigidbody2D.MovePosition(Rigidbody2D.position + Vector2.up * speed * Time.deltaTime);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "MainCircleTag")
+        {
+            transform.SetParent(collision.transform);
+            controll = true;
+        }
+
+        if (collision.gameObject.tag == "SmallCircleTag")
+        {
+            SceneManager.LoadScene(gameLevel);
+        }
+    }
+}
